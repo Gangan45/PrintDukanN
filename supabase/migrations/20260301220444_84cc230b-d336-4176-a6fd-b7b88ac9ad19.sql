9addc237a8bc94f1fdcf724bc7385f61ce79eb9e
Barun Kumar Singh <deshmukhgagan45@gmail.com>
2026-03-02 07:20:38 +0530

    cc
    c

--- supabase/migrations/20260301220444_84cc230b-d336-4176-a6fd-b7b88ac9ad19.sql
+++
@@ -1,5 +0,0 @@
-
-DROP POLICY IF EXISTS "Admins can view all profiles" ON public.profiles;
-CREATE POLICY "Admins can view all profiles" ON public.profiles
-  FOR SELECT
-  USING (has_role(auth.uid(), 'admin'::app_role));
