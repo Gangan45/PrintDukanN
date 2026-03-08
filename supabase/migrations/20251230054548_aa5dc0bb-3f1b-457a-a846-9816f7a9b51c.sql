966c4fe9e3f1d25f3aba456a74e9e9e0aaf63301
Barun Kumar Singh <pkenterprise206@gmail.com>
2026-03-09 02:37:13 +0530

    c

--- supabase/migrations/20251230054548_aa5dc0bb-3f1b-457a-a846-9816f7a9b51c.sql
+++
@@ -1,7 +0,0 @@
--- Drop and recreate the admin policy for profiles to allow admins to view ALL profiles
-DROP POLICY IF EXISTS "Admins can view all profiles" ON public.profiles;
-
-CREATE POLICY "Admins can view all profiles" 
-ON public.profiles 
-FOR SELECT 
-USING (has_role(auth.uid(), 'admin'::app_role));
--- supabase/migrations/20260308184941_611c15c1-655b-4a24-a8cf-e8279c69ec9c.sql
+++
@@ -1,8 +0,0 @@
-UPDATE products 
-SET images = ARRAY[
-  '/images/name-pencil-1.jpg',
-  '/images/name-pencil-2.jpg', 
-  '/images/name-pencil-3.jpg',
-  '/images/name-pencil-4.jpg'
-]
-WHERE id = '504b08e6-bbea-4233-aa08-51d449a6b5f4';
